2952c60b947b3c0504ef528e61b7e6cdc676f100
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-07-17 15:21:45 -0700

    add widget

---
+++ app/Http/Livewire/Math/Widget.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Math;
+
+use Livewire\Component;
+
+class Widget extends Component
+{
+    public function render()
+    {
+        return view('livewire.math.widget');
+    }
+}
