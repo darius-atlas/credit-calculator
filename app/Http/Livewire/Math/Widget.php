42127b36f26c044af840a74c1619117ca08757c3
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-07-17 16:12:21 -0700

    Add test algorithm

--- app/Http/Livewire/Math/Widget.php
+++ app/Http/Livewire/Math/Widget.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class Widget extends Component
 {
+    public $total = 0, $sum, $procentage, $month;
+
+    public function math() {
+        for($i = 0; $i <= $this->month; $i++) {
+            $thisPayment = ($this->sum / 100) * $this->procentage;
+            $this->sum -= $thisPayment;
+            $this->total += $thisPayment;
+        }
+        $this->render();
+    }
+
     public function render()
     {
         return view('livewire.math.widget');
--- resources/views/livewire/math/widget.blade.php
+++ resources/views/livewire/math/widget.blade.php
@@ -1,3 +1,19 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div>
+        <label for="month">Кол. Месяцев</label>
+        <input type="nubmer" id="month" wire:model="month">
+
+        <label for="procentage">Процентная Ставка</label>
+        <input type="nubmer" id="procentage" wire:model="procentage">
+
+        <label for="sum">Сумма</label>
+        <input type="nubmer" id="sum" wire:model="sum">
+
+        <button type="button" wire:click="math()">math</button>
+    </div>
+    <div>
+        <h4 class="text-center">
+            {{ $total }}
+        </h4>
+    </div>
 </div>
